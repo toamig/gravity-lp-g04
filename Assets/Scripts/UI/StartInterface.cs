9491ece655b068b622ece4ea44e85292e2079166
Onso37 <80353484+Onso37@users.noreply.github.com>
2022-04-01 15:44:59 +0100

    start screen

--- Assets/Scripts/UI/StartInterface.cs
+++ Assets/Scripts/UI/StartInterface.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class StartInterface : MonoBehaviour
 {
+
+    public Button StartButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartButton.onClick.AddListener(() => SceneManager.LoadScene("Level1", LoadSceneMode.Single));
     }
 
     // Update is called once per frame
